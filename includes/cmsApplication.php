02f98fd09c6bb70dd133d8497c3efc9cbbd534a2
unknown <imamriva72i@gmail.com>
2015-04-21 13:11:45 +0700

    Menambah filte template dan css

--- includes/cmsApplication.php
+++ includes/cmsApplication.php
@@ -1,4 +1,16 @@
 <?php
-require once ('cmsBase.php');
+require_once ('cmsBase.php');
 class CmsApplication extends CmsBase {
+function addcontent()
+{
+	echo 'Di sini akan dibuat fungsi menambah konten';
+}
+function viewcontent()
+{
+	echo 'Di sini akan dibuat fungsi untuk menampilkan konten';
+}
+function anyothertask()
+{
+	echo 'Di sini akan ditulis fungsi yang lainnya';
+}
 }
